e155a267b92e33ccfb99ae1809347df57dca8991
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-06-30 16:56:38 -0500

    change: rutas preparadas

--- src/controllers/estudiantes.js
+++ src/controllers/estudiantes.js
@@ -6,23 +6,26 @@ const getAll = async (req = request, res = response) => {
     res.json(result);
 };
 
-const getbyId = async (req = request, res = response, id) => {
-    const [result] = await conecction.query(`SELECT * FROM Estudiante 
-                                                WHERE id_estudiante = ${id}`);
+const getbyId = async (req = request, res = response) => {
+    const id = req.params.id;
+    const [result] = await conecction.query('SELECT * FROM Estudiante WHERE id_estudiante = ?', [id]);
+    res.json(result);
 };
 
-const putbyId = async (req = request, res = response, id) => {
-    const [result] = await conecction.query(``);
+const putbyId = async (req = request, res = response) => {
+    //const [result] = await conecction.query(`);
+    res.send("Ruta de Modificar estudiantes")
 }
 
 const postEstudiante = async (req = request, res = response)=>{
-    const [result] = await conecction.query(`INSERT INTO Estudiante(nombre, apellido, cedula, nivel, facultad_id, carrera_id, foto)
-                                                VALUES()`)
+    //const [result] = await conecction.query('INSERT INTO Estudiante(nombre, apellido, cedula, nivel, facultad_id, carrera_id, foto) VALUES()')
+    res.send("Ruta de Agregar estudiantes");
 }
 
-const dlbyId = async (req = request, res = response, id) => {
-    const [result] = await conecction.query(`DELETE FROM Estudiante 
-                                                WHERE id_estudiante = ${id}`);
+const dlbyId = async (req = request, res = response) => {
+    const id = req.params.id;
+    //const [result] = await conecction.query('DELETE FROM Estudiante WHERE id_estudiante = ?',[id]);
+    res.send("Ruta de Eliminar estudiante");
 }
 
 module.exports = {
