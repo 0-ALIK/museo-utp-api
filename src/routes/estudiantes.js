2706bc961ae018b056f327f43c31c3a738d93652
Gustavo Leoteau <Gustavoleoteau3@gmail.com>
2023-06-30 16:57:33 -0500

    change: rutas modificadas

--- src/routes/estudiantes.js
+++ src/routes/estudiantes.js
@@ -1,24 +1,16 @@
 const { Router } = require('express');
-const { getAll, dlbyId, postEstudiante, putbyId } = require('../controllers/estudiantes')
+const { getAll, getbyId, dlbyId, postEstudiante, putbyId } = require('../controllers/estudiantes')
 
 const router = Router();
 
 router.get('/all', getAll);
 
-router.get('/:id', (req, res)=>{
-    getbyId(req.params.id));
-};
+router.get('/:id', getbyId);
 
-router.put('/modificar/:id', (req, res) =>{
-    putbyId(req.params.id);
-});
+router.put('/modificar/:id', putbyId);
 
-router.post('/agregar',(req, res) => {
-    postEstudiante();
-});
+router.post('/agregar',postEstudiante);
 
-router.delete('/:id', (req, res)=>{
-    dlbyId(req.params.id);
-});
+router.delete('/:id', dlbyId);
 
 module.exports = router;
