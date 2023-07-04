d81a55c86260c95e803e6a0780c1e764398d3f66
Yunier Yau <yauyunier@gmail.com>
2023-07-03 22:16:05 -0500

    dependencia de firebase agregada y termine de setear el fireStorage

--- src/controllers/articulos.js
+++ src/controllers/articulos.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const conecction = require('../config/conecction');
+const fireStorage = require('../config/fireStorage');
 
 const getAll = async (req = request, res = response) => {
     // const [result] = await conecction.query('SELECT * FROM articulos');
@@ -8,13 +9,20 @@ const getAll = async (req = request, res = response) => {
 };
 
 const createArticle = async (req = request, res = response) => {
+   //const multimedios = req.files.multimedios;
    res.send('hola desde crear articulos POST /api/articulos')
 };
 
 const getArticle = async (req = request, res = response) => {
      const { id } = req.params;
-    // const [result] = await conecction.query('SELECT * FROM articulos WHERE id = ?', [id]);
-    // res.json(result);
+     //select f.id_multimedios, f.url as fotos, v.url as videos, a.url as audios 
+     //from multimedios as f
+     //left outer join multimedios v on f.id_multimedios = v.url AND v.tipo = 'video'
+     //left outer join multimedios a on f.id_multimedios = a.url AND a.tipo = 'audio'
+     //where f.tipo = 'foto'
+     //const query = 'SELECT id_articulos, nombre, descripcion,categoria_id, create_at, update_at,  FROM articulos join multimedios on articulos.id_articulos = multimedios.articulos_id WHERE id_articulos = ?';
+     //const [result, metadata] = await conecction.query(query, [id]);
+     //res.json(result);
     res.send('hola desde /api/articulos/:id , '+ id)
 };
 
