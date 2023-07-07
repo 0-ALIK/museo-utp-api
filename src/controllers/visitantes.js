5583ecb34d2d6fe33b9c79381e2f0b8c9d185a15
José Liao <tlh.tf2_@hotmail.com>
2023-07-06 19:35:28 -0500

    Documentación

--- src/controllers/visitantes.js
+++ src/controllers/visitantes.js
@@ -2,21 +2,23 @@ const { request, response } = require('express');
 const connection = require('../config/connection');
 const consulta = require('../helpers/consultas-helper');
 
+// Maneja una solicitud GET para recuperar información del visitante basada en un parámetro de ID
 const getVisitante = async (req = request, res = response) => {
-    const id = req.params.id;
-    const [result, metadato] = await connection.query(consulta.visitanteById, [id]);
-    res.status(200).json(result);
+  const id = req.params.id; // Extrae el parámetro ID de la solicitud
+  const [resultado, metadato] = await connection.query(consulta.visitanteById, [id]); // Ejecuta una consulta SQL para recuperar la información del visitante
+  res.status(200).json(resultado); // Envía el resultado como una respuesta JSON con un código de estado 200 (OK)
 };
 
-const postVisitante = async (req = request, res = response)=>{
-    const estudiante = req.usuarioAuth;
-    const articulo = req.params.id;
-    const [] = await connection.query(consulta.insertVisita, [estudiante.id, articulo]);
-    const [result, metadato] = await connection.query(consulta.getLastVisitante);
-    res.status(201).json(result);
-}
+// Maneja una solicitud POST para crear una nueva entrada de visitante
+const postVisitante = async (req = request, res = response) => {
+  const estudiante = req.usuarioAuth; // Obtiene el usuario autenticado de la solicitud
+  const articulo = req.params.id; // Extrae el ID del artículo de la solicitud
+  await connection.query(consulta.insertVisita, [estudiante.id, articulo]); // Ejecuta una consulta SQL para insertar los datos de la visita en la base de datos
+  const [resultado, metadato] = await connection.query(consulta.getLastVisitante); // Ejecuta una consulta SQL para recuperar la última entrada del visitante
+  res.status(201).json(resultado); // Envía el resultado como una respuesta JSON con un código de estado 201 (Creado)
+};
 
 module.exports = {
-    getVisitante,
-    postVisitante
-}
+  getVisitante,
+  postVisitante
+};
--- src/routes/visitantes.js
+++ src/routes/visitantes.js
@@ -2,16 +2,17 @@ const { Router } = require('express');
 const { getVisitante, postVisitante } = require('../controllers/visitantes');
 const { mostrarErrores, validarJWTMiddleware } = require('../middlewares');
 
-
 const router = Router();
 
+// Ruta GET para obtener información de los visitantes por ID de articulo
 router.get('/:id', [
-    mostrarErrores
+    mostrarErrores // Middleware para mostrar errores en caso de que ocurran
 ], getVisitante);
 
+// Ruta POST para crear una nueva entrada de visitante
 router.post('/:id', [
-    mostrarErrores,
-    validarJWTMiddleware
+    mostrarErrores, // Middleware para mostrar errores en caso de que ocurran
+    validarJWTMiddleware // Middleware para validar el token JWT
 ], postVisitante);
 
 module.exports = router;
